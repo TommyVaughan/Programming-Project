36a5dd3518c70dfdcdc81a29e759f5633ee3c429
TommyVaughan <35727694+TommyVaughan@users.noreply.github.com>
2018-04-19 17:25:02 +0100

    Add files via upload

---
+++ scat.py
@@ -0,0 +1,17 @@
+# Thomas Vaughan 12/04/2018.
+# Opening the Iris data set to see the data in the csv file.
+# Figured out how to use scatter plots here https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+
+
+import pandas as pd                                                               # Adding all relevant libraries 
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+import numpy as nm
+
+headers = ['Sepal Length','Sepal Width','Petal Length','Petal Width','Species']   # Adding headers to the data
+data = pd.read_csv("Data/iris.csv", names = headers) 
+
+
+scatter_matrix(data)
+plt.show()
